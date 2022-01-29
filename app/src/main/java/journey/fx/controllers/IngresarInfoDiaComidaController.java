2ed6126852aa465fd987c4cfd4a54538f87071ef
Anthony Suárez <neolight1010@gmail.com>
2022-01-28 19:49:04 -0500

    Link IngresarInfoDiaComidaPages to each other.

--- app/src/main/java/journey/fx/controllers/IngresarInfoDiaComidaController.java
+++ app/src/main/java/journey/fx/controllers/IngresarInfoDiaComidaController.java
@@ -1,10 +1,16 @@
 package journey.fx.controllers;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 import journey.core.Journey;
+import journey.fx.scenes.IngresarInfoDiaComidaPage;
+import journey.fx.scenes.LoggedInMenu;
 
 public class IngresarInfoDiaComidaController {
     @FXML
@@ -13,7 +19,15 @@ public class IngresarInfoDiaComidaController {
     @FXML
     TilePane alimentosTilePane;
 
-    public void initData(Stage stage, Journey journey, int comidaIndex) {
+    @FXML
+    Button continueBtn;
+
+    @FXML
+    private void initialize() {
+        continueBtn.setDefaultButton(true);
+    }
+
+    public void initData(Stage stage, Journey journey, int comidaIndex) throws IOException {
         if (comidaIndex < 0 || comidaIndex > 2)
             throw new IllegalArgumentException("comidaIndex must be an integer from 0 to 2.");
 
@@ -29,8 +43,29 @@ public class IngresarInfoDiaComidaController {
                 break;
         }
 
+        final Scene nextPage = getNextPage(stage, journey, comidaIndex);
+
+        // Set button action
+        continueBtn.setOnAction((e) -> {
+            stage.setScene(nextPage);
+        });
+
+        // Add alimentos to TilePane
         for (var alimento : journey.bancoAlimentos) {
             alimentosTilePane.getChildren().add(new Label(alimento.getNombre()));
         }
+    }
+
+    private Scene getNextPage(Stage stage, Journey journey, int comidaIndex) throws IOException {
+        Scene nextPage = null;
+
+        if (comidaIndex < 2) {
+            nextPage = IngresarInfoDiaComidaPage.scene(stage, journey, comidaIndex + 1);
+        }
+        else {
+            nextPage = LoggedInMenu.scene(stage, journey);
+        }
+
+        return nextPage;
     }
 }
