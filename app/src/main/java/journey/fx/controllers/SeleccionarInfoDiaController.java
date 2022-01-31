30e1cefbe9ecbeee6597d345c05f453a6b5d34ad
Anthony Suárez <neolight1010@gmail.com>
2022-01-31 11:28:02 -0500

    Add datePicker and resetBtn to SeleccionarInfoDia fxml.

--- app/src/main/java/journey/fx/controllers/SeleccionarInfoDiaController.java
+++ app/src/main/java/journey/fx/controllers/SeleccionarInfoDiaController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 import journey.core.InfoDia;
@@ -13,6 +15,12 @@ import journey.core.Journey;
 public class SeleccionarInfoDiaController {
     @FXML
     ListView<InfoDia> infoDiariaListView;
+
+    @FXML
+    DatePicker datePicker;
+
+    @FXML
+    Button resetDatePickerBtn;
 
     public void initData(Stage stage, Journey journey) {
         List<InfoDia> infoDiariaItems = new ArrayList<>();
